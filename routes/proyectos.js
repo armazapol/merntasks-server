2003dc056b7397beac3435104a07325674a2edde
Pol Armaza Trigoso <pol.armaza.002@gmail.com>
2020-09-29 16:51:48 -0500

    2

---
+++ routes/proyectos.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const proyectoController = require('../controllers/proyectoController')
+const auth = require('../middleware/auth')
+const {check} = require('express-validator')
+
+// crea proyectos
+// endpoint api/proyectos
+router.post('/',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.crearProyecto
+)
+//obtener todos los proyectos
+router.get('/',
+    auth,
+    proyectoController.obtenerProyectos
+)
+
+//actualizar proyecto
+router.put('/:id',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+)
+
+//eliminar proyecto
+router.delete('/:id',
+    auth,
+    proyectoController.eliminarProyecto
+)
+module.exports = router
