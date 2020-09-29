1989e3bf9fdb483b894fa7428c3874b66b491b43
Pol Armaza Trigoso <pol.armaza.002@gmail.com>
2020-09-29 18:01:59 -0500

    aa

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.use('/api/tareas', require('./routes/tareas'))
 
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`el servidor esta funcionando en el puerto ${PORT}`)
+    console.log(`el servidor esta funcionando en el puerto ${port}`)
 })
